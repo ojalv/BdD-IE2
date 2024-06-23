d6058c9aad363655967c39a8e3b529753ed03e1c
alvaro <oj.alv99@gmail.com>
2024-06-23 19:37:30 -0300

    comentario con instrucciones

--- 4.scripts con funciones de grupo/TIPOS-DE-SANGRE-DE-PACIENTES.sql
+++ 4.scripts con funciones de grupo/TIPOS-DE-SANGRE-DE-PACIENTES.sql
@@ -1,3 +1,7 @@
+/*para hacer esta consulta primero deben ejecutarse los siguientes scripts en el orden indicado
+1) 1.scripts de creacion de base de datos/ALTER-TABLE-HC.sql
+2) 2.scripts con todos los insert a las tablas/UPDATE-HC.sql*/
+
 select tipo_sangre as "tipo de sangre", COUNT(id_historia_clinica) as pacientes
 from HISTORIA_CLINICA
 group by tipo_sangre
